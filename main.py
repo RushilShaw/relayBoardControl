3675fcce343d6209efa394757c6da1720fae968a
Rushil Shah <shah.rushil.s@gmail.com>
2023-06-14 16:59:13 -0400

    Untested but should work code

--- main.py
+++ main.py
@@ -1,118 +1,41 @@
-import smbus2 as smbus
+import sys
+import relayController_fortesting as relayController
 
-# The number of relay ports on the relay board.
-NUM_RELAY_PORTS = 4
-
-# Change the following value if your Relay board uses a different I2C address.
-DEVICE_ADDRESS = 0x20  # 7 bit address (will be left shifted to add the read write bit)
-
-# Don't change the values, there's no need for that.
-DEVICE_REG_MODE1 = 0x06
-DEVICE_REG_DATA = 0xff
-
-bus = smbus.SMBus(1)  # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
+function_dict = {
+        "ON": relayController.relay_on,
+        "OFF": relayController.relay_off,
+        "TOGGLE": relayController.relay_toggle_port,
+        "STATUS": relayController.relay_get_port_status,
+        "DATA": relayController.relay_get_port_data
+    }
 
 
-def relay_on(relay_num):
-    global DEVICE_ADDRESS
-    global DEVICE_REG_DATA
-    global DEVICE_REG_MODE1
+def validate_inputs(arguments):
+    invalid_number_of_arguments_msg = "Must have two and only two arguments.\n"
+    invalid_first_argument_msg = "The first argument must be the which port on the relay controller.\n" \
+                                 "Starting at 1 thru NUM_RELAY_PORTS specified in relayController.py"
+    invalid_second_argument_msg = "The second argument is the command on the port specified in the first argument. " \
+                                  f"Valid commands include: {' '.join(function_dict.keys())}"
 
-    if isinstance(relay_num, int):
-        # do we have a valid relay number?
-        if 0 < relay_num <= NUM_RELAY_PORTS:
-            print('Turning relay', relay_num, 'ON')
-            DEVICE_REG_DATA &= ~(0x1 << (relay_num - 1))
-            bus.write_byte_data(DEVICE_ADDRESS, DEVICE_REG_MODE1, DEVICE_REG_DATA)
-        else:
-            print('Invalid relay #:', relay_num)
+    error_msg = ""
+    if len(arguments) != 3:
+        error_msg = invalid_number_of_arguments_msg + invalid_first_argument_msg + invalid_second_argument_msg
     else:
-        print('Relay number must be an Integer value')
+        if not arguments[1].isnumeric() or not (0 < int(arguments[1]) <= relayController.NUM_RELAY_PORTS):
+            error_msg += invalid_first_argument_msg
+        if arguments[2] not in function_dict.keys():
+            error_msg += invalid_second_argument_msg
 
-
-def relay_off(relay_num):
-    global DEVICE_ADDRESS
-    global DEVICE_REG_DATA
-    global DEVICE_REG_MODE1
-
-    if isinstance(relay_num, int):
-        # do we have a valid relay number?
-        if 0 < relay_num <= NUM_RELAY_PORTS:
-            print('Turning relay', relay_num, 'OFF')
-            DEVICE_REG_DATA |= (0x1 << (relay_num - 1))
-            bus.write_byte_data(DEVICE_ADDRESS, DEVICE_REG_MODE1, DEVICE_REG_DATA)
-        else:
-            print('Invalid relay #:', relay_num)
-    else:
-        print('Relay number must be an Integer value')
-
-
-def relay_all_on():
-    global DEVICE_ADDRESS
-    global DEVICE_REG_DATA
-    global DEVICE_REG_MODE1
-
-    print('Turning all relays ON')
-    DEVICE_REG_DATA &= ~(0xf << 0)
-    bus.write_byte_data(DEVICE_ADDRESS, DEVICE_REG_MODE1, DEVICE_REG_DATA)
-
-
-def relay_all_off():
-    global DEVICE_ADDRESS
-    global DEVICE_REG_DATA
-    global DEVICE_REG_MODE1
-
-    print('Turning all relays OFF')
-    DEVICE_REG_DATA |= (0xf << 0)
-    bus.write_byte_data(DEVICE_ADDRESS, DEVICE_REG_MODE1, DEVICE_REG_DATA)
-
-
-def relay_toggle_port(relay_num):
-    print('Toggling relay:', relay_num)
-    if relay_get_port_status(relay_num):
-        # it's on, so turn it off
-        relay_off(relay_num)
-    else:
-        # it's off, so turn it on
-        relay_on(relay_num)
-
-
-def relay_get_port_status(relay_num):
-    # determines whether the specified port is ON/OFF
-    global DEVICE_REG_DATA
-    print('Checking status of relay', relay_num)
-    res = relay_get_port_data(relay_num)
-    if res > 0:
-        mask = 1 << (relay_num - 1)
-        # return the specified bit status
-        # return (DEVICE_REG_DATA & mask) != 0
-        return (DEVICE_REG_DATA & mask) == 0
-    else:
-        # otherwise (invalid port), always return False
-        print("Specified relay port is invalid")
-        return False
-
-
-def relay_get_port_data(relay_num):
-    # gets the current byte value stored in the relay board
-    global DEVICE_REG_DATA
-    print('Reading relay status value for relay', relay_num)
-    # do we have a valid port?
-    if 0 < relay_num <= NUM_RELAY_PORTS:
-        # read the memory location
-        DEVICE_REG_DATA = bus.read_byte_data(DEVICE_ADDRESS, DEVICE_REG_MODE1)
-        # return the specified bit status
-        return DEVICE_REG_DATA
-    else:
-        # otherwise (invalid port), always return 0
-        print("Specified relay port is invalid")
-        return 0
+    if error_msg:
+        raise ValueError(error_msg)
+    return int(arguments[1]), arguments[2]
 
 
 def main():
-    pass
+    relay_number, command = validate_inputs(sys.argv)
+    function = function_dict[command]
+    function(relay_number)
 
 
 if __name__ == '__main__':
     main()
-
