0213e263996e3966a971277d942e5ae954a7f138
Rushil Shah <shah.rushil.s@gmail.com>
2023-07-12 20:56:56 -0400

    added ip address

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import socket
 import relayController
 
 
-USE_ETHERNET_INSTEAD_OF_USB = False
+USE_ETHERNET_INSTEAD_OF_USB = True
 COM_PORT = "COM14"
-IP_ADDRESS = ""
-PORT = 0
+IP_ADDRESS = "169.254.92.38"
+PORT = 8080
 BAUD = 115200
 NUM_RELAY_PORTS = 16
-function_dict = {
+command_to_function = {
         "ON": relayController.RelayController.turn_on_relay_by_index,
         "OFF": relayController.RelayController.turn_off_relay_by_index,
         "TOGGLE": relayController.RelayController.toggle_relay_by_index,
@@ -22,7 +22,7 @@ function_dict = {
 def validate_inputs(argv: list[str]) -> (str, int):
     invalid_number_of_arguments_msg = "This program must be run with two command line arguments. \n"
     invalid_first_argument_msg = "The first argument is the command that is sent to the relay board. " \
-                                 f"Valid commands include: [{' '.join(function_dict.keys())}]\n"
+                                 f"Valid commands include: [{' '.join(command_to_function.keys())}]\n"
     invalid_second_argument_msg = "The second argument must be the which port on the relay controller. " \
                                   "This number must be between 1 and NUM_RELAY_PORTS\n"
 
@@ -30,9 +30,9 @@ def validate_inputs(argv: list[str]) -> (str, int):
     if len(argv) != 3:
         error_msg = invalid_number_of_arguments_msg + invalid_first_argument_msg + invalid_second_argument_msg
     else:
-        if argv[1] not in function_dict.keys():
+        if argv[1] not in command_to_function.keys():
             error_msg += invalid_first_argument_msg
-        if not argv[2].isnumeric() or not (0 < int(argv[2]) <= NUM_RELAY_PORTS):
+        if not argv[2].isnumeric() or not (1 <= int(argv[2]) <= NUM_RELAY_PORTS):
             error_msg += invalid_second_argument_msg
 
     if error_msg:
@@ -43,7 +43,7 @@ def validate_inputs(argv: list[str]) -> (str, int):
 
 def main():
     command, relay_number = validate_inputs(sys.argv)
-    function = function_dict[command]
+    function = command_to_function[command]
 
     if USE_ETHERNET_INSTEAD_OF_USB:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -54,7 +54,7 @@ def main():
     with communication_method as combus:
         relay_controller = relayController.RelayController(combus=combus)
         response = function(relay_controller, relay_number)
-    return response
+    print(response)
 
 
 if __name__ == '__main__':
